5ab7510041c0ec54e8982c8cc7b086552a0941df
Aman <amank1909201@gmail.com>
2024-02-11 13:45:40 +0530

    made laoptops page

---
+++ src/app/laptop/page.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import Link from 'next/link'
+
+const laptop = () => {
+    return (
+        <>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4 ">
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop1'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop2'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop3'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop4'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop5'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop6'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop7'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+                        <div className="lg:w-1/5  md:w-1/2 p-4  w-full shadow-lg m-2">
+                            <Link href={'/product/hplaptop8'}>   <div className="block relative rounded overflow-hidden">
+                                <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://m.media-amazon.com/images/I/81PJ86E5cVL._AC_UY327_FMwebp_QL65_.jpg" />
+                            </div>
+                            </Link>
+                            <div className="mt-4 text-center">
+                                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">HP laptop</h3>
+                                <h2 className="text-gray-900 title-font text-lg font-medium">best in the world</h2>
+                                <p className="mt-1">₹60000</p>
+                            </div>
+                        </div>
+
+
+
+
+
+
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default laptop
