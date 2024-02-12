8f883fddfa6141a11e21e8124784a2dbba79723d
Aman <amank1909201@gmail.com>
2024-02-12 23:49:36 +0530

    changes on checkout and made clear cart

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -74,7 +74,10 @@ const Navbar = () => {
             <div className='flex items-center justify-center w-1/3'>1</div>
             </div>
           </li>
-          <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          <div className="flex">
+          <button className="flex mx-1  text-white bg-indigo-500 h-max px-2  hover:bg-indigo-600 rounded t">Checkout</button>
+          <button className="flex mx-1  text-white bg-indigo-500 h-max border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-lg">clear cart</button>
+          </div>
         </ol>
       </div>
 
