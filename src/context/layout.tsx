4b67a0853f47db759ab33b3b6f84ab6d29435540
Aman <amank1909201@gmail.com>
2024-02-23 16:16:21 +0530

    made context file for all operation

---
+++ src/context/layout.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { createContext, useState, useEffect, useContext } from "react";
+
+const Appcontext=createContext<any>(undefined);
+
+export function Appwrapper({children}:{
+    children: React.ReactNode;
+  
+  }){
+  const [cart, setcart] = useState<any>({})
+  const [subtotal, setsubtotal] = useState<number>(0)
+  
+  useEffect(() => {
+    console.log("hello bro")
+    try {
+      const cartData = localStorage.getItem("cart");
+      if (cartData !== null) {
+        setcart(JSON.parse(cartData));
+      }
+     
+    } catch (error) {
+      console.error(error)
+    }
+    
+    
+  }, [])
+  const savecart = (mycart: any): void => {
+    localStorage.setItem("cart", mycart)
+    let subt = 0
+    const keys = Object.keys(cart)
+    for (let i = 0; i < keys.length; i++) {
+      subt += mycart[keys[i]].price * mycart[keys[i]].qty
+    }
+    setsubtotal(subt)
+
+  }
+  const addtocart = (itemid: number, qty: number, price: number, name: string, size: string, varient: string): void => {
+    let newcart = cart
+    if (itemid in cart) {
+      newcart[itemid].qty = cart[itemid].qty + qty
+    }
+    else {
+      newcart[itemid] = { qty: 1, price, name, size, varient }
+    }
+    setcart(newcart)
+    savecart(newcart)
+  }
+  const removetocart = (itemid: number, qty: number, price: number, name: string, size: string, varient: string): void => {
+    let newcart = cart
+    if (itemid in cart) {
+      newcart[itemid].qty = cart[itemid].qty + qty
+    }
+    if (newcart[itemid]["qty"] <= 0) {
+      delete newcart[itemid]
+    }
+    setcart(newcart)
+    savecart(newcart)
+  }
+  const clearcart = (): void => {
+    setcart({})
+    savecart({})
+    console.log("clear cart")
+  }
+  return (
+    <>
+    <Appcontext.Provider value={{cart, addtocart, removetocart, clearcart, subtotal}}>
+        {children}
+    </Appcontext.Provider>
+    </>)
+  }
+
+  export function useAppcontext(){
+    return useContext(Appcontext)
+  }
